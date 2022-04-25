490b588b25ae463947afbc8019b7520872311cc7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-25 10:37:09 +0800

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$("#btn-save").click( function() {
-  var text = $("#textarea").val();
-  var filename = $("#input-fileName").val()
-  var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-  saveAs(blob, filename+".txt");
-});
